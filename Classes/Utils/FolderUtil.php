26930baf4358582a06ecb1b1b4c821aca5cf4cab
Thomas Rokohl <thomas.rokohl@codemacher.de>
2024-01-24 10:42:27 +0100

    BugFix getFolderInfo loop

--- Classes/Utils/FolderUtil.php
+++ Classes/Utils/FolderUtil.php
@@ -1,38 +1,41 @@
 <?php
-declare(strict_types=1);
-namespace Codemacher\TileProxy\Utils;
 
+declare(strict_types=1);
+
+namespace Codemacher\TileProxy\Utils;
 
 class FolderUtil
 {
-
-  public static function formatFilesize($bytes): string
-  {
-    if ($bytes == 0)
-      return "0.00 B";
-    $s = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
-    $e = floor(log($bytes, 1024));
-    return round($bytes / pow(1024, $e), 2) . $s[$e];
-  }
-
-  public static function getFolderInfo(string $dir): array
-  {
-    $size = 0;
-    $files = 0;
-    foreach (glob(rtrim($dir, '/') . '/*', GLOB_NOSORT) as $entry) {
-      $info = FolderUtil::getFolderInfo($entry);
-      if (is_file($entry)) {
-        $size += filesize($entry);
-        ++$files;
-      } else {
-        $info = FolderUtil::getFolderInfo($entry);
-        $files += $info["files"];
-        $size += $info["size"];
-      }
+    public static function formatFilesize(int $bytes): string
+    {
+        if ($bytes == 0) {
+            return "0.00 B";
+        }
+        $s = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        $e = floor(log($bytes, 1024));
+        return round($bytes / pow(1024, $e), 2) . $s[$e];
     }
-    return [
-      "size" => $size,
-      "files" => $files
-    ];
-  }
+
+    public static function getFolderInfo(string $dir): array
+    {
+        $size = 0;
+        $files = 0;
+        $list = glob(rtrim($dir, '/') . '/*', GLOB_NOSORT);
+        if($list) {
+            foreach ($list as $entry) {
+                if (is_file($entry)) {
+                    $size += filesize($entry);
+                    ++$files;
+                } else {
+                    $info = FolderUtil::getFolderInfo($entry);
+                    $files += $info["files"];
+                    $size += $info["size"];
+                }
+            }
+        }
+        return [
+          "size" => $size,
+          "files" => $files
+        ];
+    }
 }
